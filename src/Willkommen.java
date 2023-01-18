78e120dac66500f550f766f35ae53a156cc32131
BuildTools <unconfigured@null.spigotmc.org>
2023-01-18 13:02:40 +0100

    new version

---
+++ src/Willkommen.java
@@ -0,0 +1,16 @@
+public class Willkommen {
+
+    public static void main(String[] args) {
+        begreussung();
+    }
+
+    private static void begreussung(){
+        System.out.println("Herzlich Willkommen!");
+        System.out.println("\nWählen Sie ihre Wunschfahrkarte für Berlin AB aus:");
+        System.out.println("Kurzstrecke AB [2,00 EUR] (1)");
+        System.out.println("Einzelfahrschein AB [3,00 EUR] (2)");
+        System.out.println("Tageskarte Regeltarif AB [8,80 EUR] (3)");
+        System.out.println("4-Fahrten-Karte AB [9,40 EUR] (4)");
+    }
+
+}
